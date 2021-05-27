aefc820695dc10eaf88a80724180dbb0788d6060
Kristian Ekenes <kekenes@esri.com>
2021-05-27 16:56:59 -0700

    make slider smarter

--- app/layerListUtils.js
+++ app/layerListUtils.js
@@ -1,9 +1,48 @@
-define(["require", "exports", "esri/views/layers/support/FeatureEffect", "esri/views/layers/support/FeatureFilter", "esri/widgets/Slider"], function (require, exports, FeatureEffect, FeatureFilter, Slider) {
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+define(["require", "exports", "esri/views/layers/support/FeatureEffect", "esri/views/layers/support/FeatureFilter", "esri/widgets/Slider", "esri/smartMapping/statistics/summaryStatistics", "esri/intl"], function (require, exports, FeatureEffect, FeatureFilter, Slider, summaryStatistics, intl_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     function createFilterPanelContent(params) {
+        var _this = this;
         var panel = params.panel;
-        var fields = panel.listItem.layer.fields;
+        var layer = panel.listItem.layer;
+        var fields = layer.fields;
+        var layerView = panel.listItem.layerView;
         var panelContent = document.createElement("div");
         var fieldSelect = document.createElement("select");
         var option = document.createElement("option");
@@ -21,49 +60,87 @@ define(["require", "exports", "esri/views/layers/support/FeatureEffect", "esri/v
         });
         var slider = new Slider({
             min: 0,
-            max: 10000,
+            max: 100,
             container: document.createElement("div"),
-            values: [0],
+            values: [0, 100],
             visibleElements: {
                 labels: true,
                 rangeLabels: true
             },
             rangeLabelInputsEnabled: true,
             labelInputsEnabled: true,
-            disabled: true
-        });
-        fieldSelect.addEventListener("change", function () {
-            slider.disabled = fieldSelect.value === "";
-            var layerView = panel.listItem.layerView;
-            var field = fieldSelect.value;
-            if (layerView.effect) {
-                layerView.effect.filter = new FeatureFilter({
-                    where: field + " > " + slider.values[0]
-                });
-            }
-            else {
-                layerView.effect = new FeatureEffect({
-                    filter: new FeatureFilter({
-                        where: field + " > " + slider.values[0]
-                    }),
-                    includedEffect: "",
-                    excludedEffect: "opacity(0%)"
+            disabled: true,
+            thumbsConstrained: false,
+            labelFormatFunction: function (value) {
+                // #35436 - Implementation details concerning defaults
+                var minimumSignificantDigits = 2;
+                var suggestedMaxDigits = Math.floor(Math.log10(Math.abs(value))) + 1;
+                // Clamp 'maximumSignificantDigits' to no less than 4 (or no more than 7)... default is 4
+                var maximumSignificantDigits = suggestedMaxDigits < 4 || suggestedMaxDigits > 6 ? 4 : suggestedMaxDigits;
+                var compactThreshold = 1000000; // Inclusive; displays 999,999 then 1M
+                var notation = Math.abs(value) >= compactThreshold ? "compact" : "standard";
+                return intl_1.formatNumber(value, {
+                    notation: notation,
+                    minimumSignificantDigits: minimumSignificantDigits,
+                    maximumSignificantDigits: maximumSignificantDigits
                 });
             }
         });
+        fieldSelect.addEventListener("change", function () { return __awaiter(_this, void 0, void 0, function () {
+            var layerView, field, _a, min, max, segment, where, filter;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        slider.disabled = fieldSelect.value === "";
+                        layerView = panel.listItem.layerView;
+                        field = fieldSelect.value;
+                        return [4 /*yield*/, summaryStatistics({
+                                layer: layer,
+                                field: field
+                            })];
+                    case 1:
+                        _a = _b.sent(), min = _a.min, max = _a.max;
+                        slider.set({
+                            max: max,
+                            min: min,
+                            values: [min, max]
+                        });
+                        segment = slider.segmentElements.getItemAt(1);
+                        console.log(slider.segmentElements);
+                        segment.classList.add("included-filter");
+                        where = field + " BETWEEN " + min + " AND " + max;
+                        filter = new FeatureFilter({
+                            where: where
+                        });
+                        if (layerView.effect) {
+                            layerView.effect.filter = filter;
+                        }
+                        else {
+                            layerView.effect = new FeatureEffect({
+                                filter: filter,
+                                includedEffect: "",
+                                excludedEffect: "opacity(0%)"
+                            });
+                        }
+                        return [2 /*return*/];
+                }
+            });
+        }); });
         slider.watch("values", function (values) {
             var layerView = panel.listItem.layerView;
             var field = fieldSelect.value;
+            var min = values[0] <= values[1] ? values[0] : values[1];
+            var max = values[0] >= values[1] ? values[0] : values[1];
+            var where = field + " BETWEEN " + min + " AND " + max;
+            var filter = new FeatureFilter({
+                where: where
+            });
             if (layerView.effect) {
-                layerView.effect.filter = new FeatureFilter({
-                    where: field + " > " + values[0]
-                });
+                layerView.effect.filter = filter;
             }
             else {
                 layerView.effect = new FeatureEffect({
-                    filter: new FeatureFilter({
-                        where: field + " > " + values[0]
-                    }),
+                    filter: filter,
                     includedEffect: "",
                     excludedEffect: "opacity(0%)"
                 });
--- app/layerListUtils.ts
+++ app/layerListUtils.ts
@@ -3,6 +3,10 @@ import esri = __esri;
 import FeatureEffect = require("esri/views/layers/support/FeatureEffect");
 import FeatureFilter = require("esri/views/layers/support/FeatureFilter");
 import Slider = require("esri/widgets/Slider");
+import summaryStatistics = require("esri/smartMapping/statistics/summaryStatistics");
+import histogram = require("esri/smartMapping/statistics/histogram");
+import HistogramRangeSlider = require("esri/widgets/HistogramRangeSlider");
+import { formatNumber } from "esri/intl";
 
 interface CreateFilterPanelParams {
   fields?: esri.Field[];
@@ -11,7 +15,9 @@ interface CreateFilterPanelParams {
 
 export function createFilterPanelContent(params: CreateFilterPanelParams){
   const { panel } = params;
-  const fields = (panel.listItem.layer as esri.FeatureLayer).fields;
+  const layer = (panel.listItem.layer as esri.FeatureLayer);
+  const { fields } = layer;
+  const layerView = panel.listItem.layerView as esri.FeatureLayerView;
 
   const panelContent = document.createElement("div");
   const fieldSelect = document.createElement("select");
@@ -34,33 +40,66 @@ export function createFilterPanelContent(params: CreateFilterPanelParams){
 
   const slider = new Slider({
     min: 0,
-    max: 10000,
+    max: 100,
     container: document.createElement("div"),
-    values: [ 0 ],
+    values: [ 0, 100 ],
     visibleElements: {
       labels: true,
       rangeLabels: true
     },
     rangeLabelInputsEnabled: true,
     labelInputsEnabled: true,
-    disabled: true
+    disabled: true,
+    thumbsConstrained: false,
+    labelFormatFunction: (value) => {
+      // #35436 - Implementation details concerning defaults
+      const minimumSignificantDigits = 2;
+      const suggestedMaxDigits = Math.floor(Math.log10(Math.abs(value))) + 1;
+
+      // Clamp 'maximumSignificantDigits' to no less than 4 (or no more than 7)... default is 4
+      const maximumSignificantDigits = suggestedMaxDigits < 4 || suggestedMaxDigits > 6 ? 4 : suggestedMaxDigits;
+      const compactThreshold = 1000000; // Inclusive; displays 999,999 then 1M
+      const notation = Math.abs(value) >= compactThreshold ? "compact" : "standard";
+
+      return formatNumber(value, {
+        notation,
+        minimumSignificantDigits,
+        maximumSignificantDigits
+      } as any);
+    }
   });
 
-  fieldSelect.addEventListener("change", () => {
+  fieldSelect.addEventListener("change", async () => {
     slider.disabled = fieldSelect.value === "";
 
     const layerView = panel.listItem.layerView as esri.FeatureLayerView;
     const field = fieldSelect.value;
 
+    const { min, max } = await summaryStatistics({
+      layer,
+      field
+    });
+
+    slider.set({
+      max,
+      min,
+      values: [ min, max ]
+    });
+
+    const segment = slider.segmentElements.getItemAt(1);
+    console.log(slider.segmentElements);
+    segment.classList.add("included-filter");
+
+    const where = `${field} BETWEEN ${min} AND ${max}`;
+    const filter = new FeatureFilter({
+      where
+    });
+
     if(layerView.effect){
-      layerView.effect.filter = new FeatureFilter({
-        where: `${field} > ${slider.values[0]}`
-      });
+      layerView.effect.filter = filter;
     } else {
       layerView.effect = new FeatureEffect({
-        filter: new FeatureFilter({
-          where: `${field} > ${slider.values[0]}`
-        }),
+        filter,
         includedEffect: ``,
         excludedEffect: `opacity(0%)`
       });
@@ -71,15 +110,19 @@ export function createFilterPanelContent(params: CreateFilterPanelParams){
     const layerView = panel.listItem.layerView as esri.FeatureLayerView;
     const field = fieldSelect.value;
 
+    const min = values[0] <= values[1] ? values[0] : values[1];
+    const max = values[0] >= values[1] ? values[0] : values[1];
+
+    const where = `${field} BETWEEN ${min} AND ${max}`;
+    const filter = new FeatureFilter({
+      where
+    });
+
     if(layerView.effect){
-      layerView.effect.filter = new FeatureFilter({
-        where: `${field} > ${values[0]}`
-      });
+      layerView.effect.filter = filter;
     } else {
       layerView.effect = new FeatureEffect({
-        filter: new FeatureFilter({
-          where: `${field} > ${values[0]}`
-        }),
+        filter,
         includedEffect: ``,
         excludedEffect: `opacity(0%)`
       });
